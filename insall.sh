dcf76e695ddca093db9d43cb53f4d8097b9ae067
cccaternberg <acaternberg@cloudbees.com>
2024-07-03 08:57:04 +0200

    add

---
+++ insall.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+
+NAMESPACE=cloudbees-core
+#see https://docs.cloudbees.com/docs/cloudbees-ci/latest/cloud-admin-guide/kubernetes-self-signed-certificates
+
+
+kubectl api-versions | grep admissionregistration.k8s.io/v1
+
+#copy certs
+kubectl cp cjoc-0:etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem ./ca-certificates.crt
+kubectl cp cjoc-0:etc/pki/ca-trust/extracted/java/cacerts ./cacerts
+
+#Add root CA to system certificate bundle:
+#cat mycertificate.pem >> ca-certificates.crt
+
+#Add root CA to Java cacerts:
+#keytool -import -noprompt -keystore cacerts -file mycertificate.pem -storepass changeit -alias service-mycertificate;
+
+kubectl delete configmap ca-bundles -n $NAMESPACE
+kubectl create configmap --from-file=ca-certificates.crt,cacerts ca-bundles -n $NAMESPACE
+
+kubectl create namespace cloudbees-sidecar-injector
+
+helm repo update
+helm upgrade -i cloudbees-sidecar-injector cloudbees/cloudbees-sidecar-injector --namespace cloudbees-sidecar-injector
+
+kubectl --namespace cloudbees-sidecar-injector get all
+
+
+kubectl label namespace $NAMESPACE sidecar-injector=enabled
+
+kubectl get namespace -L sidecar-injector
+kubectl apply -f testpod.yaml -n $NAMESPACE
+kubectl describe pod example-pod  -n $NAMESPACE |grep -o  /etc/ssl/ca-bundle.pem
