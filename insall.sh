079b5355368a5431b52992e4ff09c87409a4f1c1
cccaternberg <acaternberg@cloudbees.com>
2024-10-29 08:54:47 +0100

    add patch

--- insall.sh
+++ insall.sh
@@ -6,6 +6,7 @@ NAMESPACE=cloudbees-core
 
 kubectl api-versions | grep admissionregistration.k8s.io/v1
 
+kubectl create ns $NAMESPACE
 #copy certs
 kubectl cp -n $NAMESPACE  cjoc-0:etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem ./ca-certificates.crt
 kubectl cp -n $NAMESPACE  cjoc-0:etc/pki/ca-trust/extracted/java/cacerts ./cacerts
@@ -18,21 +19,31 @@ kubectl cp -n $NAMESPACE  cjoc-0:etc/pki/ca-trust/extracted/java/cacerts ./cacer
 
 kubectl delete configmap ca-bundles -n $NAMESPACE
 kubectl create configmap --from-file=ca-certificates.crt,cacerts ca-bundles -n $NAMESPACE
-
+#helm uninstall  cloudbees-sidecar-injector --namespace cloudbees-sidecar-injector
 kubectl create namespace cloudbees-sidecar-injector
 helm repo update
 #helm fetch cloudbees/cloudbees-sidecar-injector --untar
 helm upgrade -i cloudbees-sidecar-injector cloudbees/cloudbees-sidecar-injector --namespace cloudbees-sidecar-injector
 kubectl --namespace cloudbees-sidecar-injector get all
 
+# We use this patch to make sidecar injection implicit. Other wise it will break certmanager CRD
+# see https://docs.cloudbees.com/docs/cloudbees-ci/latest/cloud-admin-guide/kubernetes-self-signed-certificates#_making_injection_explicit
+kubectl patch configmap  cloudbees-sidecar-injector  -n cloudbees-sidecar-injector \
+ --type='json' -p='[{"op": "replace", "path": "/data/sidecarconfig.yaml", "value": "annotationPrefix: com.cloudbees.sidecar-injector\nrequiresExplicitInjection: true"}]'
+
+
 kubectl label namespace $NAMESPACE sidecar-injector=enabled
 kubectl get namespace -L sidecar-injector
 
+# Explicit injection, see https://docs.cloudbees.com/docs/cloudbees-ci/latest/cloud-admin-guide/kubernetes-self-signed-certificates#_making_injection_explicit
 kubectl -n $NAMESPACE apply -f  - <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
   name: example-pod
+  annotations:
+    cluster-autoscaler.kubernetes.io/safe-to-evict: "false"
+    com.cloudbees.sidecar-injector/inject: "true"
 spec:
   containers:
     - name: example-container
